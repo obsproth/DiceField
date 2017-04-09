36939797788ef4b6cee115e5645662cd9923ef29
obsproth <obsproth@users.noreply.github.com>
2017-04-09 11:49:53 +0900

    Add grid

--- xifield/XIField.py
+++ xifield/XIField.py
@@ -1,22 +1,32 @@
 import pygame
+import numpy as np
 import sys
 from time import sleep
 
-BLACK = (0,) * 3
 WHITE = (255,) * 3
 
 class XIField:
-    def __init__(self, size):
+    def __init__(self, size, grid, bg_color, grid_color):
+        self.size = size
         self.screen = pygame.display.set_mode(size)
+        self.bg_color = bg_color
+        self.grid = grid
+        self.grid_color = grid_color
         self.pos = [0, 0]
     def main(self):
-        self.screen.fill(BLACK)
+        self.screen.fill(self.bg_color)
+        for x in range(self.grid[0]):
+            pos_x = x * self.size[0] // self.grid[0]
+            pygame.draw.line(self.screen, self.grid_color, (pos_x, 0), (pos_x, self.size[1]))
+        for y in range(self.grid[1]):
+            pos_y = y * self.size[1] // self.grid[1]
+            pygame.draw.line(self.screen, self.grid_color, (0, pos_y), (self.size[0], pos_y))
         pygame.draw.rect(self.screen, WHITE, (*self.pos, 20, 20))
         pygame.display.flip()
 
 def main():
     pygame.init()
-    field = XIField((500, 500))
+    field = XIField((500, 500), (5, 5), (0,) * 3, (255,) * 3)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
